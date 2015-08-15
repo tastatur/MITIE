b5d782213510a50e529822599d21930f57e078f9
tastatur <repkov.denis@gmail.com>
2015-08-15 16:13:21 +0200

    Training scripts

---
+++ train-german/train-tiger.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python3.4 ./train.py -o $HOME/masterarbeit-data/mitte/training-out/tiger-mt-model.dat -i $HOME/masterarbeit-data/mitte/corpora/tiger.train -f $HOME/masterarbeit-data/mitte/features-output/total_word_feature_extractor.dat
